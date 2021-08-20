a065087194db361dd6fbcd2922eb207202354163
sauzuniga <88364351+sauzuniga@users.noreply.github.com>
2021-08-20 16:09:17 -0600

    Agregar archivos de proyecto.

---
+++ TablasDeMultiplicaciones/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TablasDeMultiplicar;
+namespace TablasDeMultiplicaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string respuesta = "si";
+            while (respuesta == "si")
+            {
+                Multiplicar.MultiCollection();
+                Console.WriteLine(" Desea generar otra tabla de multiplicar diferente si/no ");
+                respuesta = Console.ReadLine();
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TablasDeMultiplicar/Multiplicar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TablasDeMultiplicar
+{
+    public class Multiplicar
+    {
+        public static void MultiCollection()
+        {
+            
+            int num, i;
+            Console.WriteLine(" Digite la tabla de multiplicar del numero que desea visualizar por favor ");
+            num = int.Parse(Console.ReadLine());
+            for(i=1;i<=10;i++)
+            {
+                Console.WriteLine("{0} x {1} = {2} ", num, i, (num * i));
+            }
+        }
+    }
+}
